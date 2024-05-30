14498d2a0d0d025f05f87f0e3f743f0a2a1fbe11
lapisprincess <lernertilda@gmail.com>
2024-05-30 15:41:19 -0700

    Added dependency for the lopdf package

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 use std::collections::HashMap;
 
+use lopdf::Document;
+
 pub mod keyword_parser;
+
+
+fn parse_pdf() {
+    let mut doc = Document::load("")
+}
 
 fn main() {
     let keywords: HashMap<String, u32> = keyword_parser::process_keywords(
@@ -11,4 +18,6 @@ fn main() {
         let (key, score) = entry;
         println!("{}: {}", key, score);
     }
+
+
 }
