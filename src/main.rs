b2971a298cf3e2e3e579c1c45b26337d5ac7d6ac
lapisprincess <lernertilda@gmail.com>
2024-05-30 16:01:56 -0700

    Worked on reading PDF's, good progress

--- src/main.rs
+++ src/main.rs
@@ -6,18 +6,31 @@ pub mod keyword_parser;
 
 
 fn parse_pdf() {
-    let mut doc = Document::load("")
+    let doc = match Document::load("bulletin_example.pdf") {
+        Ok(d) => d,
+        Err(_) => {
+            println!("File not found!");
+            return;
+        }
+    };
+    println!("{}", doc.extract_text(&[231]).unwrap());
 }
 
 fn main() {
-    let keywords: HashMap<String, u32> = keyword_parser::process_keywords(
+    let keywords: HashMap<String, u32> = match keyword_parser::process_keywords(
         String::from("tests/test_keywords.txt")
-    ).unwrap();
+    ) {
+        Ok(k) => k,
+        Err(_) => {
+            println!("File not found!");
+            return;
+        }
+    };
 
     for entry in keywords {
         let (key, score) = entry;
         println!("{}: {}", key, score);
     }
 
-
+    parse_pdf();
 }
